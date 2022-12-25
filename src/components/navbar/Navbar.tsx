ce5d9ef16ffb87792837358aef35a7a54d629905
erencelik <erenmemo05@gmail.com>
2022-12-25 12:41:07 +0300

    changed font-weight

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <header className="h-[35px] bg-black flex justify-center items-center">
-      <h1 className="text-cyan-300 font-mono text-2xl">Hangman</h1>
+      <h1 className="text-cyan-300 font-mono text-2xl font-medium">Hangman</h1>
     </header>
   );
 };
