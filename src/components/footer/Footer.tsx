da70bc7d253a31e22b863a6fac5d32bd01cb08cb
erencelik <erenmemo05@gmail.com>
2022-12-25 12:41:50 +0300

    styled main and footer components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-[35px] w-full bg-black text-cyan-300">
+      <div className="container mx-auto py-2">
+        <div className="flex items-center justify-center text-xs">
+          <div className="flex items-center pl-2">
+            <p>Copyright © 2022 </p>
+            <a
+              href="https://github.com/av-erencelik"
+              className="pl-1 underline hover:opacity-70"
+            >
+              av-erencelik
+            </a>
+            .
+          </div>
+          <p className="pl-1">
+            Created with{" "}
+            <a
+              href="https://www.wordnik.com"
+              className=" underline hover:text-gray-400"
+              target="_blank"
+            >
+              Wordnik
+            </a>{" "}
+            API
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/main/Game.tsx
+++ src/components/main/Game.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Game = () => {
-  return <div className="flex-1">Main</div>;
+  return (
+    <div className="flex-1 bg-cyan-300 flex justify-center items-center">
+      <button className="bg-black text-cyan-300 p-2 px-5 rounded-sm hover:bg-cyan-300 hover:text-black transition-all border-2 border-black font-semibold">
+        START
+      </button>
+    </div>
+  );
 };
 
 export default Game;
